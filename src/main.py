879e4c562816b23c55daf4a6a0bc0672b890f9ee
Antonov Sergey <serg2009141.antono@yandex.ru>
2021-10-12 20:51:39 +0300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python2.7
+import rospy
+from geometry_msgs.msg import Twist
+from math import atan2, pi
+from turtlesim.msg import Pose
+
+
+def msg(x, z):
+    twist = Twist()
+    twist.linear.x = x
+    twist.angular.z = z
+    return twist
+
+
+class PoseEstimation:
+    current_pose = Pose()
+    target_pose = Pose()
+
+    def update_current(cls, new_pose):
+        cls.current_pose = new_pose
+
+    def update_target(cls, new_pose):
+        cls.target_pose = new_pose
+
+    def distance(cls):
+        delta_pose = cls.target_pose.x - cls.current_pose.x, cls.target_pose.y - cls.current_pose.y
+        return (delta_pose[0]**2 + delta_pose[1]**2)**0.5
+
+    def angle(cls):
+        return (atan2(cls.target_pose.y - cls.current_pose.y, cls.target_pose.x - cls.current_pose.x) - cls.current_pose.theta + pi) % (2*pi) - pi
+
+    def __str__(cls):
+        return "current_pose x, y: {} {}, target_pose x, y: {} {}, distance: {}, angle: {}".format(cls.current_pose.x, cls.current_pose.y, cls.target_pose.x, cls.target_pose.y, cls.distance(), cls.angle())
+
+
+if __name__ == "__main__":
+
+    min_distance = 1
+
+    rospy.init_node('turtle_follower')
+    rate = rospy.Rate(2)
+
+    pos = PoseEstimation()
+    
+    pub = rospy.Publisher('/turtle_follower/cmd_vel', Twist, queue_size=1)
+    sub_follower = rospy.Subscriber(
+        '/turtle_follower/pose', Pose, pos.update_current)
+    sub_following = rospy.Subscriber(
+        '/turtle1/pose', Pose, pos.update_target)
+
+    while not rospy.is_shutdown():
+        dist = pos.distance()
+        rospy.loginfo(str(pos))
+        if dist >= min_distance:
+            angle = pos.angle()
+            tw = msg(dist, angle)
+            pub.publish(tw)
+        rate.sleep()

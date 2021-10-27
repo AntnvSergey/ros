43a1f54c681a02c960c3ccb166544b09180fad1f
Antonov Sergey <serg2009141.antono@yandex.ru>
2021-10-27 20:34:48 +0300

    add lab2

---
+++ src/lab2/src/main.py
@@ -0,0 +1,87 @@
+#! /usr/bin/python3
+
+import rospy
+import numpy as np
+from geometry_msgs.msg import Pose
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import OccupancyGrid, MapMetaData
+
+
+class Occupancy_grid:
+    def __init__(self):
+        self.grid = 0
+        self.grid_size = 300
+        self.grid_center = [150, 150]
+        self.scale = self.grid_size / 10
+
+    def draw_points(self, ranges, angles, data):
+
+        for i, laser_data in enumerate(ranges):
+
+            x = laser_data * np.sin(angles[i]) * self.scale + self.grid_center[0]
+            y = laser_data * np.cos(angles[i]) * self.scale + self.grid_center[1]
+
+            if abs(x) < data.shape[0] and abs(y) < data.shape[1]:
+                data[abs(int(x))][abs(int(y))] = 200
+
+                coord = np.array([abs(int(x)), abs(int(y))])
+                delta = coord - np.array(self.grid_center)
+                dist = np.linalg.norm(delta)
+                route = delta / np.linalg.norm(delta)
+
+                if dist < np.inf:
+                    for i in range(int(dist)):
+                        point = np.array(self.grid_center + route * i).astype(int)
+                        if point[0] < data.shape[0] and point[1] < data.shape[1]:
+                            data[point[0]][point[1]] = 0
+                        else:
+                            break
+
+    def create_grid(self, msg):
+        ranges = msg.ranges
+        angle_min = msg.angle_min
+        angle_max = msg.angle_max
+        angle_increment = msg.angle_increment
+
+        angles = np.arange(angle_min, angle_max, angle_increment)
+
+        data = np.full((self.grid_size, self.grid_size), fill_value=-1)
+        self.draw_points(ranges, angles, data)
+
+        self.grid = np.flipud(data).flatten().astype(np.int8).tolist()
+
+
+if __name__ == "__main__":
+    rospy.init_node('occupancy_grid_creator')
+    rate = rospy.Rate(1)
+    Grid = Occupancy_grid()
+
+    pub = rospy.Publisher('/occupancy_grid', OccupancyGrid, queue_size=1)
+    sub_scan = rospy.Subscriber(
+        '/base_scan', LaserScan, Grid.create_grid)
+
+    min_distance = 1
+    while not rospy.is_shutdown():
+        if Grid.grid:
+            meta_data = MapMetaData()
+            meta_data.resolution = 1 / Grid.scale
+            # meta_data.width = Grid.data_shape[1]
+            # meta_data.height = Grid.data_shape[0]
+
+            meta_data.width = Grid.grid_size
+            meta_data.height = Grid.grid_size
+
+            map_msg = OccupancyGrid()
+            map_msg.data = Grid.grid
+            map_msg.header.frame_id = '/base_laser_link'
+
+            # pos = np.array([-meta_data.width * meta_data.resolution / 2, -
+            #                meta_data.height * meta_data.resolution / 2, 0])
+            pos = np.array([0, 0, 0])
+            meta_data.origin = Pose()
+            meta_data.origin.position.x, meta_data.origin.position.y = pos[:2]
+            map_msg.info = meta_data
+
+            rospy.loginfo('publish created grid')
+            pub.publish(map_msg)
+        rate.sleep()
